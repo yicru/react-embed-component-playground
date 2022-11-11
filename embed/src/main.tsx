9845894700444049bc3ad664e30a6377ddb9a62c
yuichiro miyamae <yu1ro.mya@gmail.com>
2022-11-11 19:39:43 +0900

    feat: embed

--- embed/src/main.tsx
+++ embed/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const App = () => {
+  return (
+    <React.StrictMode>
+      <img src="https://placehold.jp/150x150.png" alt="" />
+    </React.StrictMode>
+  );
+};
+
+export const init = (target: string) => {
+  const container = document.getElementById(target);
+  if (!container) {
+    throw new Error("Target Element not found");
+  }
+  const root = createRoot(container);
+  root.render(<App />);
+};
--- embed/vite.config.ts
+++ embed/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/main.tsx",
+      formats: ["umd"],
+      name: "Embed",
+      fileName: "embed",
+    },
+  },
+  define: {
+    "process.env": {},
+  },
+});
